10cbf1b9d5925f2072e14204a8de035fa6650a79
Matheus <matheusfcc2010@hotmail.com>
2019-10-23 22:54:50 -0300

    adding mutex, threads cond and concurrent specific logic

--- integral_conc.c
+++ integral_conc.c
@@ -7,6 +7,12 @@ double err_max; // valor maximo do erro da integral
 double integral = 0.0; // valor da integral
 
 pthread_mutex_t mutex;
+pthread_cond_t cond_e, cond_f; // condição para as threads quando a pilha está vazia e cheia, respectivamente
+int nthreads; // número de threads que irão trabalhar
+int thread_esperando = 0; // número de threads que estão bloqueadas com cond_e
+int integral_finalizada = 0; // boolean para indicar se a integral foi finalizada, que é depois
+                            // de todas as threads terem sido bloqueadas menos a ultima e esta
+                            // saiu do loop de calcular a integral
 
 // Estrutura para guardar o início e fim de um intervalo de função
 typedef struct INTERVALO {
@@ -69,7 +75,7 @@ void pop(Pilha *pilha) {
     pilha->top--;
 }
 
-// Pega o intervalo do topo da pilha
+// Retorna o intervalo do topo da pilha
 Intervalo peek(Pilha *pilha) {
     if (isEmpty(pilha)) {
         Intervalo vazio;
@@ -93,8 +99,18 @@ void calculaIntegral(Pilha *intervalos) {
     double ret_a, ret_b, ret_c; // Área do retângulo maior e dos retângulos menores, respectivamente
     double diff; // Diferença da área entre o retângulo maior e os retângulos menores
 
+    pthread_mutex_lock(&mutex);
+    // pode ser que a thread tenha entrado na função para calcular a integral mas outras threads
+    // pegaram todos os intervalos restantes e não sobrou nenhum para essa thread calcular
+    if (isEmpty(intervalos)) {
+        pthread_mutex_unlock(&mutex);
+        return;
+    }
+
     interv = peek(intervalos);
     pop(intervalos);
+    pthread_cond_signal(&cond_f);
+    pthread_mutex_unlock(&mutex);
 
     p_medio_a = (interv.b + interv.a) / 2;
     p_medio_b = (p_medio_a + interv.a) / 2;
@@ -108,20 +124,53 @@ void calculaIntegral(Pilha *intervalos) {
     if(diff < 0) diff = diff * -1.0;
 
     if(diff < err_max) {
+        pthread_mutex_lock(&mutex);
         integral += ret_b + ret_c;
+        pthread_mutex_unlock(&mutex);
     } else {
         novos[0].a = interv.a;
         novos[0].b = p_medio_a;
         novos[1].a = p_medio_a;
         novos[1].b = interv.b;
 
+        pthread_mutex_lock(&mutex);
+
+        // pode ser que a pilha tenha ficado cheia durante os calculos da thread, então
+        // ela deve ser bloqueada até alguma outra thread retirar um intervalo da pilha
+        while(isFull(intervalos)) {
+            pthread_cond_wait(&cond_f, &mutex);
+        }
         push(intervalos, novos[0]);
         push(intervalos, novos[1]);
+        pthread_cond_broadcast(&cond_e);
+        pthread_mutex_unlock(&mutex);
     }
 }
 
 void *integra(void *intervalos) {
+    intervalos = (Pilha *)intervalos;
 
+    while(!isEmpty(intervalos)) {
+        calculaIntegral(intervalos);
+
+        // se a thread entrar nesse if quer dizer que não tem mais nenhum intervalo na
+        // pilha para se analisar, porém pode ser que outra thread esteja analisando um
+        // e pode ser que ela bote novos intervalos na pilha, indicando que a integral
+        // não tenha acabado ainda. Entretanto, para indicar que tenha acabado bloqueia-se
+        // todas as threads menos a última, sendo que se realmente acabou ela sai do loop e
+        // desbloqueia as outras threads.
+        pthread_mutex_lock(&mutex);
+        while(!integral_finalizada && isEmpty(intervalos) && thread_esperando < nthreads - 1) {
+            thread_esperando++;
+            pthread_cond_wait(&cond_e, &mutex);
+            thread_esperando--;
+        }
+        pthread_mutex_unlock(&mutex);
+    }
+    integral_finalizada = 1;
+    pthread_cond_broadcast(&cond_e);
+
+    pthread_exit(NULL);
 }
 
 // Para o cálculo de uma integral a partir de um intervalo e erro dado e por meio do método
@@ -134,7 +183,6 @@ int main(int argc, char *argv[]) {
     Intervalo inicial; // Guarda o intervalo inicial dado pelo usuário
     Pilha *intervalos = init(100); // Inicializando uma pilha de intervalos com 100 espaços
     pthread_t *tid_sistema; // threads que irão realizar o cálculo da integral
-    int nthreads; // número de threads que irão trabalhar
     int i;
 
     if(argc < 5) {
@@ -157,6 +205,10 @@ int main(int argc, char *argv[]) {
     tid_sistema = (pthread_t *)malloc(nthreads * sizeof(pthread_t));
     if(tid_sistema == NULL) { printf("Erro de malloc nas threads\n"); exit(EXIT_FAILURE); }
 
+    pthread_mutex_init(&mutex, NULL);
+    pthread_cond_init(&cond_e, NULL);
+    pthread_cond_init(&cond_f, NULL);
+
     for(i = 0; i < nthreads; i++) {
         if(pthread_create(&tid_sistema[i], NULL, integra, (void *) intervalos)) {
             printf("Erro de criação de threads\n");
@@ -164,11 +216,22 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    while(!isEmpty(intervalos)) {
-        calculaIntegral(intervalos);
+    for(i = 0; i < nthreads; i++) {
+        if(pthread_join(tid_sistema[i], NULL)) {
+            printf("Erro de espera das threads\n");
+            exit(EXIT_FAILURE);
+        }
     }
 
+    pthread_mutex_destroy(&mutex);
+    pthread_cond_destroy(&cond_e);
+    pthread_cond_destroy(&cond_f);
+
     printf("O valor da integral é aproximadamente: %.5lf\n", integral);
+
+    free(tid_sistema);
+    free(intervalos->array);
+    free(intervalos);
 
     return 0;
 }
